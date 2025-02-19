505f93ef068853fff6a2ac8c2543a8a3c6b0fbc8
Victor Tran <vicr12345@gmail.com>
2025-02-19 20:11:11 +1100

    Implement body and method for fetch

--- src/main/java/net/bnbdiscord/borderkit/jsSupport/HttpFetchProxy.java
+++ src/main/java/net/bnbdiscord/borderkit/jsSupport/HttpFetchProxy.java
@@ -7,9 +7,12 @@ import org.graalvm.polyglot.proxy.ProxyExecutable;
 import org.graalvm.polyglot.proxy.ProxyObject;
 
 import javax.net.ssl.HttpsURLConnection;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.nio.ByteBuffer;
+import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 
 public class HttpFetchProxy implements ProxyExecutable {
@@ -35,23 +38,54 @@ public class HttpFetchProxy implements ProxyExecutable {
 
                     var followRedirects = true;
                     var method = "GET";
+                    byte[] output = null;
+                    var headers = new HashMap<String, String>();
                     if (arguments.length > 1) {
                         var options = arguments[1];
-                        if (options.hasMember("followRedirects")) {
-                            followRedirects = options.getMember("followRedirects").asBoolean();
+                        if (options.hasMember("redirect")) {
+                            var redirect = options.getMember("redirect");
+                            followRedirects = redirect.asString().equals("follow");
                         }
 
                         if (options.hasMember("method")) {
                             method = options.getMember("method").asString();
                         }
+
+                        if (options.hasMember("headers")) {
+                            var jsHeaders = options.getMember("headers");
+                            for (var header : jsHeaders.getMemberKeys()) {
+                                headers.put(header, jsHeaders.getMember(header).asString());
+                            }
+                        }
+
+                        if (options.hasMember("body")) {
+                            var body = options.getMember("body");
+                            if (body.isString()) {
+                                output = StandardCharsets.UTF_8.encode(body.asString()).array();
+                            } else if (body.hasBufferElements()) {
+                                output = body.as(ByteBuffer.class).array();
+                            }
+                        }
                     }
 
-                    connection.setRequestProperty("User-Agent", "borderkit/1.0");
                     connection.setInstanceFollowRedirects(followRedirects);
                     connection.setRequestMethod(method);
+                    connection.setRequestProperty("User-Agent", "borderkit/1.0");
+                    for (var header : headers.keySet()) {
+                        if (header.equals("Host")) continue;
+                        connection.setRequestProperty(header, headers.get(header));
+                    }
 
                     if (connection instanceof HttpsURLConnection) {
                         var httpsConnection = (HttpsURLConnection) connection;
+                    }
+
+                    if (output != null) {
+                        connection.setRequestProperty("Content-Length", String.valueOf(output.length));
+                        connection.setDoOutput(true);
+                        try (DataOutputStream outputStream = new DataOutputStream(connection.getOutputStream())) {
+                            outputStream.write(output);
+                        }
                     }
 
                     connection.getResponseCode();
